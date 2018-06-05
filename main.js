04ffd8b146b5bf4504ebceadf8ad690752303416
Shuaib Sajid <shusajid@gmail.com>
2018-06-05 09:13:36 -0500

    file creation, completed lighting exercises

---
+++ main.js
@@ -0,0 +1,9 @@
+// Write a function that takes two arguments. This function should return the concatenated value of the two arguments.
+const concatenator = function(arg1,arg2){
+let combined = arg1 +" "+ arg2;
+return combined;
+}
+// 2. Call the function with your first name and last name as the two arguments.
+const myName =concatenator("Shuaib","Sajid")
+// 3. Console log the return value.
+console.log(myName)
